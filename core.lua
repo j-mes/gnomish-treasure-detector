ca1c021ef5d3cbd999f910ac943f1a0711c12699
James Loveridge <j-mes@users.noreply.github.com>
2022-11-07 15:58:06 +0000

    Tidy up things

--- core.lua
+++ core.lua
@@ -1,7 +1,7 @@
 -- rVignette: core
 -- zork, 2018
 -- Neav (fix)
--- Shamelessly stolen by Howl
+-- Used by Howl
 
 -----------------------------
 -- Variables
@@ -19,11 +19,6 @@ local function OnVignetteAdded(self,event,id)
   if self.vignettes[id] then return end
   local vignetteInfo = C_VignetteInfo.GetVignetteInfo(id)
   if not vignetteInfo then return end
-  --[[local filename, width, height, txLeft, txRight, txTop, txBottom = C_Texture.GetAtlasInfo(vignetteInfo.atlasName)
-  if not filename then return end
-  local atlasWidth = width/(txRight-txLeft)
-  local atlasHeight = height/(txBottom-txTop)
-  ]] -- Old stuff from Zork
   
   -- Adding fix from Neav
   local atlasInfo = C_Texture.GetAtlasInfo(vignetteInfo.atlasName)
@@ -32,9 +27,7 @@ local function OnVignetteAdded(self,event,id)
   local top = atlasInfo.topTexCoord * 256
   local bottom = atlasInfo.bottomTexCoord * 256
   local str = "|TInterface\\MINIMAP\\ObjectIconsAtlas:0:0:0:0:256:256:"..(left)..":"..(right)..":"..(top)..":"..(bottom).."|t"
-  -- More old stuff
-  --local str = string.format("|T%s:%d:%d:0:0:%d:%d:%d:%d:%d:%d|t", filename, 0, 0, atlasWidth, atlasHeight, atlasWidth*txLeft, atlasWidth*txRight, atlasHeight*txTop, atlasHeight*txBottom)
-  --PlaySoundFile("Sound\\Interface\\RaidWarning.ogg") -- Deprecated
+
   PlaySoundFile(567397) 
   
   
@@ -50,7 +43,6 @@ end
 -- Init
 -----------------------------
 
---eventHandler
 local eventHandler = CreateFrame("Frame")
 eventHandler:RegisterEvent("VIGNETTE_MINIMAP_UPDATED")
 eventHandler:SetScript("OnEvent", OnVignetteAdded)
