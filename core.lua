e090de080695591d9a60abca81f246bef849ddcd
j-mes <dev@j-m.es>
2024-09-04 12:37:01 +0100

    Filter: Brann Bronzebeard

--- core.lua
+++ core.lua
@@ -6,43 +6,44 @@
 -----------------------------
 -- Variables
 -----------------------------
-
 local A, L = ...
 
 -----------------------------
 -- Functions
 -----------------------------
+local function OnVignetteAdded(self, event, id)
+    if not id then return end
+    self.vignettes = self.vignettes or {}
+    if self.vignettes[id] then return end
+    local vignetteInfo = C_VignetteInfo.GetVignetteInfo(id)
+    if not vignetteInfo then return end
 
-local function OnVignetteAdded(self,event,id)
-  if not id then return end
-  self.vignettes = self.vignettes or {}
-  if self.vignettes[id] then return end
-  local vignetteInfo = C_VignetteInfo.GetVignetteInfo(id)
-  if not vignetteInfo then return end
-  
-  -- Adding fix from Neav
-  local atlasInfo = C_Texture.GetAtlasInfo(vignetteInfo.atlasName)
-  local left = atlasInfo.leftTexCoord * 256
-  local right = atlasInfo.rightTexCoord * 256
-  local top = atlasInfo.topTexCoord * 256
-  local bottom = atlasInfo.bottomTexCoord * 256
-  local str = "|TInterface\\MINIMAP\\ObjectIconsAtlas:0:0:0:0:256:256:"..(left)..":"..(right)..":"..(top)..":"..(bottom).."|t"
+    -- Adding fix from Neav
+    local atlasInfo = C_Texture.GetAtlasInfo(vignetteInfo.atlasName)
+    local left = atlasInfo.leftTexCoord * 256
+    local right = atlasInfo.rightTexCoord * 256
+    local top = atlasInfo.topTexCoord * 256
+    local bottom = atlasInfo.bottomTexCoord * 256
+    local str = "|TInterface\\MINIMAP\\ObjectIconsAtlas:0:0:0:0:256:256:" ..
+        (left) .. ":" .. (right) .. ":" .. (top) .. ":" .. (bottom) .. "|t"
 
-  PlaySoundFile(567397) 
-  
-  
-  if vignetteInfo.name ~= "Garrison Cache" and vignetteInfo.name ~= "Full Garrison Cache" and vignetteInfo.name ~= nil then
-  RaidNotice_AddMessage(RaidWarningFrame, str.." "..vignetteInfo.name.." spotted!", ChatTypeInfo["RAID_WARNING"])
-  print(str.." "..vignetteInfo.name,"spotted!")
-  self.vignettes[id] = true
-   
-end
+    PlaySoundFile(567397)
+
+    if vignetteInfo.name ~= "Garrison Cache"
+        and vignetteInfo.name ~= "Full Garrison Cache"
+        and vignetteInfo.name ~= "Brann Bronzebeard"
+        and vignetteInfo.name ~= nil
+    then
+        RaidNotice_AddMessage(RaidWarningFrame, str .. " " .. vignetteInfo.name .. " spotted!",
+            ChatTypeInfo["RAID_WARNING"])
+        print(str .. " " .. vignetteInfo.name, "spotted!")
+        self.vignettes[id] = true
+    end
 end
 
 -----------------------------
 -- Init
 -----------------------------
-
 local eventHandler = CreateFrame("Frame")
 eventHandler:RegisterEvent("VIGNETTE_MINIMAP_UPDATED")
 eventHandler:SetScript("OnEvent", OnVignetteAdded)
